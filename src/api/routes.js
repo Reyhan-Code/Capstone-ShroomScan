ac397d15aef20060674f7a8464513254e027043c
Chernoboysss <Mhmdfadillah1@gmail.com>
2024-06-19 21:35:33 +0700

    add private api

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,25 +1,77 @@
 const MushroomHandler = require('./handler');
+const validApiKeys = [process.env.KEY]; 
 
 const routes = [
     {
         method: 'GET',
         path: '/mushrooms',
-        handler: MushroomHandler.getAllMushroomHandler,
+        handler: async (request, h) => {
+            const apiKey = request.headers['api-key'];
+            if (!apiKey || !validApiKeys.includes(apiKey)) {
+              return h.response({ message: 'Invalid API key' }).code(401);
+            }
+            try {
+              const response = await MushroomHandler.getAllMushroomHandler(request, h);
+              return response;
+            } catch (error) {
+              console.error('Error handling request:', error);
+              return h.response({ message: 'Internal server error' }).code(500);
+            }
+          }
     },
     {
         method: 'GET',
         path: '/mushrooms/{jamurId}',
-        handler: MushroomHandler.getMushroombyIdHandler,
+        // handler: MushroomHandler.getMushroombyIdHandler,
+        handler: async (request, h) => {
+          const apiKey = request.headers['api-key'];
+          if (!apiKey || !validApiKeys.includes(apiKey)) {
+            return h.response({ message: 'Invalid API key' }).code(401);
+          }
+          try {
+            const response = await MushroomHandler.getMushroombyIdHandler(request, h);
+            return response;
+          } catch (error) {
+            console.error('Error handling request:', error);
+            return h.response({ message: 'Internal server error' }).code(500);
+          }
+        }
     },
     {
         method: 'GET',
         path: '/recipes',
-        handler: MushroomHandler.getAllRecipeHandler,
+        // handler: MushroomHandler.getAllRecipeHandler,
+        handler: async (request, h) => {
+          const apiKey = request.headers['api-key'];
+          if (!apiKey || !validApiKeys.includes(apiKey)) {
+            return h.response({ message: 'Invalid API key' }).code(401);
+          }
+          try {
+            const response = await MushroomHandler.getAllRecipeHandler(request, h);
+            return response;
+          } catch (error) {
+            console.error('Error handling request:', error);
+            return h.response({ message: 'Internal server error' }).code(500);
+          }
+        }
     },
     {
         method: 'GET',
         path: '/recipes/{resepId}',
-        handler: MushroomHandler.getAllRecipebyIdHandler,
+        // handler: MushroomHandler.getAllRecipebyIdHandler,
+        handler: async (request, h) => {
+          const apiKey = request.headers['api-key'];
+          if (!apiKey || !validApiKeys.includes(apiKey)) {
+            return h.response({ message: 'Invalid API key' }).code(401);
+          }
+          try {
+            const response = await MushroomHandler.getAllRecipebyIdHandler(request, h);
+            return response;
+          } catch (error) {
+            console.error('Error handling request:', error);
+            return h.response({ message: 'Internal server error' }).code(500);
+          }
+        }
     },
     {
         method: 'GET',
